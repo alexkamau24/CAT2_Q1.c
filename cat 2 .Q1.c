8254c5ad18461dbf1b09f437c135f11108f4b426
alexkamau24 <alkaminea@gmail.com>
2024-11-12 09:08:23 +0300

    cat 2 .Q1.c

--- cat 2 .Q1.c
+++ cat 2 .Q1.c
@@ -1,12 +1,14 @@
+//
 #include <stdio.h>
 #include <string.h>
+//structure to represent an employee
 struct employee {
-char name[25];
- int id;
- char department[20];
- float salary;
-    char email[50];};
-int main() {
+char name[25];//emplyee's name maximum 24 characters
+ int id;//id number
+ char department[20];//department maximum 19 characters
+ float salary;//employee's salary 
+   char email[50];};//email mmaximum 49 characters
+int main() {//initiaizing given data
 struct employee emp = {"John Doe", 12345, "Human Resources", 55000.50, "john.doe@company.com"};
 printf("Name: %s\n", emp.name);
  printf("ID: %d\n", emp.id);
